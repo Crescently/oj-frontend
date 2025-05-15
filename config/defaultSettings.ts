af91d64c388f6c4f6ebd622008f8c0ee85f6e223
Crescentlymon <2376355613@qq.com>
2025-05-15 10:51:12 +0800

    增加背景图片

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -12,6 +12,13 @@ const Settings: ProLayoutProps & {
   fixSiderbar: true,
   colorWeak: false,
   title: '在线判题平台',
+  bgLayoutImgList: [
+    {
+      src: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/V-_oS6r-i7wAAAAAAAAAAAAAFl94AQBr',
+      width: '100%',
+      height: '100%',
+    },
+  ],
   pwa: true,
   iconfontUrl: '',
   token: {
